e09175dede6614cea3ff2782ab988acbc74b8c0d
John Ewart <john@johnewart.net>
2019-08-20 19:07:33 -0700

    Update release

--- release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-mkdir -p ~/.gem
-echo "---\r\n:rubygems_api_key: $RUBYGEMS_API_KEY" > ~/.gem/credentials
-chmod 0600 ~/.gem/credentials
-gem build yourbase-rspec-skipper.gemspec
-gem push *.gem
