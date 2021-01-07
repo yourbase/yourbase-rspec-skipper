1f448858fed678487b38884deff0035f634616a3
Christina Austin <christina@yourbase.io>
2021-01-07 08:31:04 -0800

    remove everthing except for requiring the private gem

--- lib/yourbase/rspec/skipper.rb
+++ lib/yourbase/rspec/skipper.rb
@@ -1,38 +1,7 @@
-module YourBase
-  module RSpec
-    module Skipper 
-      @@injected = false
-
-      def self.inject!
-        unless @@injected
-          ::YourBase::RSpec::Skipper.init!
-          @@injected = true
-        end
-      end
-
-    end
-  end
-end
 
 begin
-  using_rspec = false
-
-  begin 
-    require 'rspec/core'
-    using_rspec = true
-  rescue LoadError => e
-    puts "YourBase can't accelerate RSpec because it's not loaded!"
-    return
-  end
-
-  begin
-    puts "Loading YourBase RSpec Skipper..."
-    require 'yourbase_test_skipper'
-    ::YourBase::RSpec::Skipper.inject!
-  rescue LoadError => e
-    puts "Failed to find or load RSpec accelerator, falling back to normal behavior"
-  end
-
+  puts "Loading YourBase RSpec accelerator..."
+  require 'yourbase_test_skipper'
 rescue => e
-  puts "Unable to load RSpec accelerator: #{e}"
+  puts "Unable to load RSpec accelerator with error: #{e}"
 end
